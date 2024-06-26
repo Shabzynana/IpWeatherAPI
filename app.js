041a6a2c0dbf4a5a4da05f18f4b070cf8b97ceca
MAC <horlarmihleykan10@gmail.com>
2024-06-26 16:59:40 +0100

    vercel added

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
 const express = require('express');
 const app = express();
-app.set('trust proxy', true)
 
-// app.get('api/hellovisitor_name/:name', (req, res) => {
-//     const client_ip = req.ip;
-//     const {name} = req.params;
-//     // const username = req.query.username || 'Unknown User';  // Get the username from query parameters
-//     res.json({ client_ip: client_ip, 'greeting':  `Hello, ${name}`, name });
-// });
+
+app.set('trust proxy', true)
 
 
 app.get('/api/hello', (req, res) => {
     const visitorName = req.query.visitor_name || 'Guest';
-    const client_ip = req.ip;
+    const client_ip = req.headers['x-forwarded-for'] || req.ip;;
     const greeting = `Hello, ${visitorName}!`;
     res.json({ client_ip: client_ip, greeting: greeting });
 });
