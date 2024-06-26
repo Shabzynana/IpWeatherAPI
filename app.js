9931fc3b0c120e68e21d5d013c2e21cdd6e6078c
MAC <horlarmihleykan10@gmail.com>
2024-06-26 17:16:23 +0100

    route updated

--- app.js
+++ app.js
@@ -5,11 +5,17 @@ const app = express();
 app.set('trust proxy', true)
 
 
-app.get('/api/hello', (req, res) => {
-    const visitorName = req.query.visitor_name || 'Guest';
-    const client_ip = req.headers['x-forwarded-for'] || req.ip;;
-    const greeting = `Hello, ${visitorName}!`;
-    res.json({ client_ip: client_ip, greeting: greeting });
+app.get('/api/hello', (req, res, next) => {
+
+    try{
+        const visitorName = req.query.visitor_name || 'Guest';
+        const client_ip = req.headers['x-forwarded-for'] || req.ip;;
+        const greeting = `Hello, ${visitorName}!`;
+        res.json({ client_ip: client_ip, greeting: greeting });
+    } catch (error) {
+        next(error);
+    }
+    
 });
 
 const PORT = process.env.PORT || 3000;
