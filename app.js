09680c953296c9198e67bc25da85c41fd6374690
MAC <horlarmihleykan10@gmail.com>
2024-06-30 23:24:51 +0100

    to get location and temperature

--- app.js
+++ app.js
@@ -1,20 +1,39 @@
 const express = require('express');
 const cors = require('cors');
-const app = express();
+const axios = require('axios');
+const app = express();require
+('dotenv').config();
 
 
+// &ip=${client_ip}
 app.use(cors());
 app.set('trust proxy', true)
 
+const apiKey = process.env.api_Key;
+const weatherApiKey = process.env.weatherApi_Key;
 
-
-app.get('/api/hello', (req, res, next) => {
+app.get('/api/hello', async (req, res, next) => {
 
     try{
-        const visitorName = req.query.visitor_name || 'Guest';
         const client_ip = req.headers['x-forwarded-for'] || req.ip;;
-        const greeting = `Hello, ${visitorName}!`;
-        res.json({ client_ip: client_ip, greeting: greeting });
+
+        const visitorName = req.query.visitor_name || 'Guest';
+
+
+        // Get location data from IPGeolocation API
+        const response = await axios.get(`https://api.ipgeolocation.io/ipgeo?apiKey=${apiKey}`);
+        const location = response.data;
+
+        // Get weather data from OpenWeatherMap API
+        const weatherResponse = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&units=metric&appid=${weatherApiKey}`);
+        const weather = weatherResponse.data;
+
+        const greeting = `Hello, ${visitorName}!, the temperature is ${weather.main.temp} degrees Celsius in ${location.city}.`;
+
+        res.json({ client_ip: client_ip,          
+            location: location.city,
+            greeting: greeting,
+         });
     } catch (error) {
         next(error);
     }
